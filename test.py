8b5d7b8e8fda44852ccbfc020de1381453168cad
Waley Chen <49982721+waley94@users.noreply.github.com>
2021-11-08 22:15:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -16,5 +16,18 @@ def hovedprogram():
     print("Antall prosessorer:", abel.antProsessorer())
     print("Antall rack:", abel.antRacks())
 
+    # Oppretter RegneKlynge og noder.
+    saga = Regneklynge(8)
+    for e in range(264):
+        saga.settInnNode(Node(64, 1))
+    for i in range(8):
+        saga.settInnNode(Node(1024, 2))
+    # Printer ut kriteriene.
+    print("Noder med minst 32 GB:", saga.noderMedNokMinne(32))
+    print("Noder med minst 64 GB:", saga.noderMedNokMinne(64))
+    print("Noder med minst 128 GB:", saga.noderMedNokMinne(128))
+    print("Antall prosessorer:", saga.antProsessorer())
+    print("Antall rack:", saga.antRacks())
+
 
 hovedprogram(
